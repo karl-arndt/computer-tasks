f44e2d67279b52ecfcefe950839444fd4b3a2054
karndt95 <karndt1995@gmail.com>
2024-06-22 16:11:32 -0400

    Add Brave installation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int brave() {
-
+void brave() {
+  system("winget install --id Brave.Brave --location C:\\test --silent");
 }
 
 int main() {
   system("rmdir C:\\test");
   system("mkdir C:\\test");
-
+  brave();
   return 0;
 }
